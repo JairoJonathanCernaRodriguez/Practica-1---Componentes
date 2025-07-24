d50597d04e988d1038cec92274f3815f14006d8c
Jairo Jonathan Cerna Rodrigues <s19120157@alumnos.itsur.edu.mx>
2025-07-24 13:23:16 -0600

    UseEffect Mejorado

--- src/app/API/page.tsx
+++ src/app/API/page.tsx
@@ -1,53 +1,66 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import {
-  Button, Card, CardBody, CardHeader, CardFooter,
-  CardTitle, CardText, Table
-} from 'reactstrap';
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardText,
+  Table,
+} from "reactstrap";
 
-import { User } from '../interfaces';
-import { Product } from '../interfaces';
+import { User, Product } from "../interfaces";
 
 export default function API() {
   const [user, setUser] = useState<User | null>(null);
   const [products, setProducts] = useState<Product[]>([]);
+  const [userLoad, setUserLoad] = useState<boolean>(false);
 
   const fetchRandomUser = async () => {
     try {
-      const res = await fetch('https://fakestoreapi.com/users');
+      const res = await fetch("https://fakestoreapi.com/users");
       const data: User[] = await res.json();
       const index = Math.floor(Math.random() * data.length);
       setUser(data[index]);
     } catch (error) {
-      console.error('Error fetching user:', error);
+      console.error("Error fetching user:", error);
     }
   };
 
   const fetchProducts = async () => {
     try {
-      const res = await fetch('https://fakestoreapi.com/products');
+      const res = await fetch("https://fakestoreapi.com/products");
       const data: Product[] = await res.json();
 
       // Simulate different 15 products
-      const randomProducts = data
-        .sort(() => 0.5 - Math.random())
-        .slice(0, 15);
+      const randomProducts = data.sort(() => 0.5 - Math.random()).slice(5, 15);
 
       setProducts(randomProducts);
     } catch (error) {
-      console.error('Error fetching products:', error);
+      console.error("Error fetching products:", error);
     }
   };
 
   useEffect(() => {
+    fetchRandomUser();
+  }, [userLoad]);
+
+  useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [userLoad]);
 
   return (
     <div className="p-4">
       <h2 className="mb-4">Random User</h2>
-      <Button color="success" onClick={fetchRandomUser}>
+      <Button
+        color="success"
+        onClick={() => {
+          setUserLoad(!userLoad);
+        }}
+      >
         Show random user
       </Button>
 
@@ -62,7 +75,8 @@ export default function API() {
             <CardText>Phone: {user.phone}</CardText>
           </CardBody>
           <CardFooter>
-            Address: {user.address.street} #{user.address.number}, {user.address.city}, ZIP {user.address.zipcode}
+            Address: {user.address.street} #{user.address.number},{" "}
+            {user.address.city}, ZIP {user.address.zipcode}
           </CardFooter>
         </Card>
       )}
